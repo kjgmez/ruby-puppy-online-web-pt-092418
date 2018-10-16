6dcc5cdaea33c09fcb01ed36043e568fe90384f0
Kevin J Gomez <kjgmez1@gmail.com>
2018-10-16 23:57:48 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,11 +2,12 @@ class Dog
   @@all = []
   def initialize(name)
     @name = name
+    @@all << self
   end
   def self.clear_all
     @@all.clear
   end
   def self.all
-    @@all
+    puts @name
   end
 end
